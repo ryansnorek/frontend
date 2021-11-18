091990f3f589165e85a1655e21f661e3ab7a53bc
Ryan <snorekr@gmail.com>
2021-11-18 12:48:21 -0500

    View

--- src/components/View.js
+++ src/components/View.js
@@ -1,14 +1,40 @@
-
+import tokenAuthorization from "../utils/tokenAuthorization";
+import { BASE_URL } from "../config";
+import { useState } from "react";
 
 const View = () => {
+    const [items, setItems] = useState([]);
+
+    tokenAuthorization()
+        .get(`${BASE_URL}/items`)
+        .then(res => {
+            setItems(res.data)
+        })
+        .catch(err => console.log(err))
+    // {/* [GET] https://african-marketplace-2.herokuapp.com/api/items */}
+    // {/* [GET] https://african-marketplace-2.herokuapp.com/api/items/:item_id */}
+    // {/* [POST] https://african-marketplace-2.herokuapp.com/api/items */}
+    // {/* [DELETE] https://african-marketplace-2.herokuapp.com/api/items/:item_id */}
+    // {/* [PUT] https://african-marketplace-2.herokuapp.com/api/items/:item_id */}
+
     return (
         <div className="view">
-            <h1>View</h1>
-            {/* [GET] https://african-marketplace-2.herokuapp.com/api/items */}
-            {/* [GET] https://african-marketplace-2.herokuapp.com/api/items/:item_id */}
-            {/* [POST] https://african-marketplace-2.herokuapp.com/api/items */}
-            {/* [DELETE] https://african-marketplace-2.herokuapp.com/api/items/:item_id */}
-            {/* [PUT] https://african-marketplace-2.herokuapp.com/api/items/:item_id */}
+            {items.map(item => {
+                return (
+                    <div className="item">
+                        <div className="left">
+                            <img src={item.img} alt={item.name} />
+                        </div>
+                        <div className="right">
+                            <h3>{item.name}</h3>
+                            <p>{item.description}</p>
+                            <p>${item.price}</p>
+                        </div>
+                    </div>
+                )
+            })}
+
+          
         </div>
     );
 };
