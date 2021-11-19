019ae449f33cda9a628148698e1b600efa942669
djho57 <daniel.j.ho57@gmail.com>
2021-11-19 14:49:02 -0600

    yup and signup

--- src/components/FormSchema.js
+++ src/components/FormSchema.js
@@ -1,24 +1,11 @@
 import * as yup from 'yup';
 
 const formSchema = yup.object().shape({
-    name: yup
-        .string()
-        .trim()
-        .required('Name is required')
-        .matches(/[a-zA-Z]/, 'Your name can only contain letters.'),
-    email: yup
-        .string()
-        .email('Enter a valid address')
-        .required('Email is required'),
     password: yup
         .string()
         .required('Password is required')
-        .min(6, 'Enter your PASSWORD HERE (must be 6 characters or more)')
-        .matches(/[a-zA-Z]/, "Password can only contain letters"),
+        .min(3, 'Enter your PASSWORD HERE (must be 3 characters or more)'),
     username: yup
         .string().required('Username is required'),
-    serviceTerms: yup
-        .boolean()
-        .oneOf([true], 'Must accepts terms')
 })
 export default formSchema;
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,37 +1,84 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import * as yup from 'yup';
+import formSchema from "./FormSchema";
+
+const initialDisabled = true;
+const initialUsers = [];
+const initialFormVals ={
+    username: '',
+    password: '',
+  }
+const initialFormErrors ={
+    username: '',
+    password: '',
+}
 
 const Signup = () => {
-    const [signup, setSignup] = useState({ username:"", password:"" });
-    const [error, setError] = useState(false);
-    // const [isLoading, setIsLoading] = useState(false);
+    const [ users, setUsers ] = useState(initialUsers)
+    const [ formVals, setFormVals ] = useState(initialFormVals)
+    const [ formErrors, setFormErrors ] = useState(initialFormErrors)
+    const [ disabled, setDisabled ] = useState(initialDisabled)
 
-    const handleChange = e => {
-        setSignup({ ...signup, [e.target.name]: e.target.value })
-    };
-    const handleSubmit = e => {
-        e.preventDefault();
-        // setIsLoading(true);
-    };
+    const postNewUser = newUser => {
+        axios.post('https://african-marketplace-2.herokuapp.com/api/auth/signup', newUser)
+        .then(res=>{
+          setUsers([...users, res.data])
+        })
+        .catch(err=>{
+          console.error(err)
+        })
+        .finally(() => setFormVals(initialFormVals))
+        }
+    const updateForm = (inputName, inputValue) => {
+        validate(inputName, inputValue)
+        setFormVals({ ...formVals, [inputName]:inputValue})
+    }
+    const submitForm = () => {
+        const newUser ={
+          username: formVals.username.trim(),
+          password: formVals.password.trim()
+        }
+        postNewUser(newUser)
+      }
+      const validate = (name, value) =>{
+        yup.reach(formSchema,name)
+        .validate(value)
+        .then(() => setFormErrors({...formErrors, [name]: ''}))
+        .catch(error => setFormErrors({...formErrors, [name]: error.errors[0]}))
+      }
+    
+    //   useEffect(() => {
+    //     getUsers()
+    //   }, [])
+      
+      useEffect(() => {
+        formSchema.isValid(formVals).then(valid => setDisabled(!valid))
+      }, [formVals])
+    
     return (
         <div className="login signup">
-             <form onSubmit={handleSubmit}>
-                <input
-                    name="username"
-                    placeholder="username"
-                    value={signup.username}
-                    onChange={handleChange}
-                />
-                <input
-                    name="password"
-                    placeholder="password"
-                    value={signup.password}
-                    onChange={handleChange}
-                />
-                <button>Sign up</button>
-            </form>
-            {error ? <p style={{color: "red"}}>invalid login</p> : ""}
+        <form onSubmit={this.handleSubmit} update={updateForm} submit={submitForm} disabled={disabled} errors={formErrors}>
+            <span className="formtext">Sign up</span>
+            <input
+            type='text'
+            name='username'
+            placeholder='username'
+            value={this.state.username}
+            onChange={event => this.setState({username: event.target.value})}
+            />
+            <input
+            type='password'
+            name='password'
+            placeholder='password'
+            value={this.state.password}
+            onChange={event => this.setState({password: event.target.value})}
+            />
+            <button>Sumbit</button>
+        </form>
         </div>
     )
-};
+    }
+
 
 export default Signup;
